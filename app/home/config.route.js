82a7e3926ec26a14d74af33fabd3493cea91d972
Kenneth Tam <ktam@purdue.edu>
2016-06-20 15:05:41 -0400

    Access mutantList.html (comment out controller as it is unfinished at this state)

--- app/home/config.route.js
+++ app/home/config.route.js
@@ -5,16 +5,16 @@
     .module('mutantApp.home', [])
     .config(configFunction);
 
-    // Config sets up state for the home section
-    configFunction.$inject = ['$stateProvider'];
+  // Config sets up state for the home section
+  configFunction.$inject = ['$stateProvider'];
 
-    // Using state provider so when the url is just '/#/' (after the port)
-    // it references home.html
-    // .../#/ in a url references angular stuff or something
-    function configFunction($stateProvider) {
-      $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'app/home/home.html'
-      });
-    }
+  // Using state provider so when the url is just '/#/' (after the port)
+  // it references home.html
+  // .../#/ in a url references angular stuff or something
+  function configFunction($stateProvider) {
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'app/home/home.html'
+    });
+  }
 })();
---
+++ app/mutantList/mutantList.controller.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.mutantList')
+    .controller('MutantListController', MutantListController);
+
+
+})();
