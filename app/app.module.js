4102708cd77812c2a4d2933bfb07c0ad41ff6727
Kenneth Tam <ktam@purdue.edu>
2016-06-20 13:46:00 -0400

    Create main app module

---
+++ app/app.module.js
@@ -0,0 +1,13 @@
+// IIFE
+(function() {
+  'use strict';
+
+  angular.module('mutantApp', [
+    // Angular modules
+    'ui.router',
+
+    // Third-party modules
+    'firebase',
+    
+  ]);
+})();
