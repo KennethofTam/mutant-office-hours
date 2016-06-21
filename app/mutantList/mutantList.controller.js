f36cc09fbf5b1854659ef052c126c10bbfd0cf82
Kenneth Tam <ktam@purdue.edu>
2016-06-21 14:19:17 -0400

    adds ng-click to text button and creates reference to text in Firebase that gets added to via send text function

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -11,12 +11,15 @@
     var vm = this;
     // var fireMutantsRef = new Firebase('https://mutant-school.firebaseio.com/');
     var mutantsRef = firebase.database().ref().child('mutants');
+    var textsRef = firebase.database().ref().child('texts');
+
 
     vm.addMutant = addMutant;
     vm.mutants =  $firebaseArray(mutantsRef); // ['deadpool', 'nightcrawler', 'gambit'];
     vm.newMutant = new Mutant();
     vm.deleteMutant = deleteMutant;
     vm.toggleComplete = toggleComplete;
+    vm.sendText = sendText;
 
     // We don't use vm.name because we're not referencing the controller (?)
     function Mutant() {
@@ -35,11 +38,29 @@
     }
 
     function deleteMutant(mutant) {
-      //Premade delete method part of angularfire
+      //Premade $remove method part of angularfire
       vm.mutants.$remove(mutant);
     }
 
     function toggleComplete(mutant) {
+      vm.mutants.$save(mutant);
+    }
+
+    function sendText(mutant) {
+      // build text object
+      var newText = {
+        name:mutant.name,
+        phone:mutant.phone,
+        topic:mutant.topic
+      }
+
+      // save text to firebase
+      textsRef.push(newText);
+
+      // change modified to true
+      mutant.notified = true;
+
+      // save mutant
       vm.mutants.$save(mutant);
     }
   }
