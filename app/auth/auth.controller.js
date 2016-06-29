a1d77061607c0cf0404c55c702bd62a85803e3a3
Kenneth Tam <ktam@purdue.edu>
2016-06-29 15:14:31 -0400

    Started condensing login and register forms into a directive, added to index

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -11,43 +11,43 @@
     var vm = this;
     //var auth = $firebaseAuth();
 
-    vm.register = register;
-    vm.login = login;
-    vm.error = null;
-    // vm.logout = logout;
-    // vm.isLoggedIn = authService.isLoggedIn;
-
-    vm.user = {
-      email: '',
-      password: ''
-    }
-
-    function register(user) {
-      return authService.register(user) //auth.$createUserWithEmailAndPassword(user.email, user.password)
-        //.then(function(user) {
-        // User passed to the function in the above line is not the user object
-        // that login expects
-        .then(function() {
-          vm.login(user);
-
-        })
-        .then(function() {
-          return authService.sendWelcomeEmail(user.email);
-        })
-        .catch(function(error) {
-          vm.error = error;
-        });
-    }
-
-    function login(user) {
-      return authService.login(user) //auth.$signInWithEmailAndPassword(user.email, user.password)
-        .then(function(user) {
-          $state.go('mutantList');
-        })
-        .catch(function(error) {
-          vm.error = error;
-        });
-    }
+    // vm.register = register;
+    // vm.login = login;
+    // vm.error = null;
+    // // vm.logout = logout;
+    // // vm.isLoggedIn = authService.isLoggedIn;
+    //
+    // vm.user = {
+    //   email: '',
+    //   password: ''
+    // }
+    //
+    // function register(user) {
+    //   return authService.register(user) //auth.$createUserWithEmailAndPassword(user.email, user.password)
+    //     //.then(function(user) {
+    //     // User passed to the function in the above line is not the user object
+    //     // that login expects
+    //     .then(function() {
+    //       vm.login(user);
+    //
+    //     })
+    //     .then(function() {
+    //       return authService.sendWelcomeEmail(user.email);
+    //     })
+    //     .catch(function(error) {
+    //       vm.error = error;
+    //     });
+    // }
+    //
+    // function login(user) {
+    //   return authService.login(user) //auth.$signInWithEmailAndPassword(user.email, user.password)
+    //     .then(function(user) {
+    //       $state.go('mutantList');
+    //     })
+    //     .catch(function(error) {
+    //       vm.error = error;
+    //     });
+    // }
     //
     // function logout() {
     //   authService.logout(); //auth.$signOut();
